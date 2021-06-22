4e3e4f355a54d30c02e6df23c92c7e77a6ef6a43
Suraj Deshmukh <SurajD@citiustech.com>
2021-06-22 21:00:33 +0530

    interceptor and css changes

--- hospital-management-FE/src/app/interceptors/HeaderInterceptor.ts
+++ hospital-management-FE/src/app/interceptors/HeaderInterceptor.ts
@@ -5,8 +5,16 @@ import { map, filter } from 'rxjs/operators';
 
 @Injectable()
 export class HeaderInterceptor implements HttpInterceptor {
-  intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const API_KEY = '123456';
-    return next.handle(httpRequest.clone({ setHeaders: { API_KEY } }));
-  }
+    intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const API_KEY = '123456';
+        const NO_HEADER = 'NA';
+        const token = localStorage.getItem("jwtToken");
+        const token1 = token?.slice(1, token.length - 1);
+        let tokenStr: string = 'Bearer ' + token1;
+        const Authorization = tokenStr;
+        if (httpRequest.url.toString().indexOf('authenticate')) {
+            return next.handle(httpRequest.clone({ setHeaders: { NO_HEADER } }));
+        } else
+            return next.handle(httpRequest.clone({ setHeaders: { Authorization } }));
+    }
 }
