c21d35e3fc8d623601487ce3893ccbd1e39a1265
Suraj Deshmukh <SurajD@citiustech.com>
2021-07-24 10:26:53 +0530

    patient role changes

--- hospital-management-FE/src/app/appoinements/appoinements.component.ts
+++ hospital-management-FE/src/app/appoinements/appoinements.component.ts
@@ -12,9 +12,22 @@ export class AppoinementsComponent implements OnInit {
   constructor(private apiService: RegistrationService) { }
 
   weekklyAppointmentList: any;
-  hasAppointmentsToday:Boolean = false;
+  hasAppointmentsToday: Boolean = false;
+  role: any;
 
   ngOnInit(): void {
+
+    this.role = sessionStorage.getItem("role");
+    if (this.role == 'A') {
+      this.role = "ADMIN";
+    } else if (this.role == 'D') {
+      this.role = "PHYSICIAN";
+    } else if (this.role == 'N') {
+      this.role = "NURSE";
+    } else {
+      this.role = "PATIENT";
+    }
+
     this.apiService.getWeeklyAppointments().subscribe(data => {
       console.log("status is " + data.status);
       console.log("weekly apointments api called");
